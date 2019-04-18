c784fa993c6a114ad2dac1f0e5eeae2e649a8dc9
Eduardo <eduardomarques@outlook.com>
2019-04-18 13:51:15 -0300

    :rocket:  User registration

--- Snapchat/CadastroViewController.swift
+++ Snapchat/CadastroViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class CadastroViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirm: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,5 +22,39 @@ class CadastroViewController: UIViewController {
     //Method called whenever the user view the screen
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true) //Hides or displays navigation bar
+    }
+    
+    @IBAction func createAccount(_ sender: Any) {
+        //Recover typed data
+        if let emailR = self.email.text {
+            if let passwordR = self.password.text {
+                if let confirmR = self.confirm.text {
+                    
+                    //Validate password
+                    if passwordR == confirmR {
+                        //Create firebase account
+                        let auth = Auth.auth()
+                        auth.createUser(withEmail: emailR, password: passwordR) { (user, error) in
+                            if error == nil{
+                                print("Sucesso ao cadastrar")
+                            } else {
+                                print("erro ao cadastrar")
+                            }
+                        }
+                    } else {
+                        self.viewMessage(title: "Senhas distintas", message: "As senhas não são iguais, digite novamente")
+                    }
+                }
+            }
+        }
+    }
+    
+    //Displayed alert to user
+    func viewMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
     }
 }
