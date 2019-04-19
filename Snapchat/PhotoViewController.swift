86da3a4b49bcc3a748929b2a35fda923a4e3b106
Eduardo <eduardomarques@outlook.com>
2019-04-19 12:22:08 -0300

    :rocket: Settings - save photo in firebase

--- Snapchat/PhotoViewController.swift
+++ Snapchat/PhotoViewController.swift
@@ -14,13 +14,19 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var desc: UITextField!
     @IBOutlet weak var nextButton: UIButton!
+    
     //Object used to allow user to take photo or select from gallery
     var imagePicker = UIImagePickerController()
+    var imageId = NSUUID().uuidString //Generates a unique identifier for each image
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imagePicker.delegate = self //This class manages photo selection
+        
+        //Disable next button
+        nextButton.isEnabled = false
+        nextButton.backgroundColor = UIColor.gray
     }
     
     @IBAction func selectPhoto(_ sender: Any) {
@@ -37,10 +43,15 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
         
         //Recover image
         if let selectedImage = image.image {
-            if let imageData = selectedImage.jpegData(compressionQuality: 0.8) {
-                images.child("imagem.jpg").putData(imageData, metadata: nil) { (metadata, error) in
+            if let imageData = selectedImage.jpegData(compressionQuality: 0.9) {
+                images.child("\(self.imageId).jpg").putData(imageData, metadata: nil) { (metaData, error) in
                     if error == nil {
                         print("Sucesso ao fazer upload do arquivo")
+                        
+                        //Download URL
+                        images.downloadURL(completion: { (url, error) in
+                            print(url?.absoluteString)
+                        })
                         
                         self.nextButton.isEnabled = true //Turn on button
                         self.nextButton.setTitle("Próximo", for: .normal)
@@ -56,6 +67,10 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let recoveredImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         image.image = recoveredImage
+        
+        //Enable next button
+        self.nextButton.isEnabled = true
+        self.nextButton.backgroundColor = UIColor(red: 0.553, green: 0.369, blue: 0.749, alpha: 1)
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
