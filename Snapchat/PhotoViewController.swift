3d9f12159aa30a55ad920738fe05a7bdf06e7fc8
Eduardo <eduardomarques@outlook.com>
2019-04-19 02:57:49 -0300

    :rocket: Select photos

--- Snapchat/PhotoViewController.swift
+++ Snapchat/PhotoViewController.swift
@@ -8,23 +8,29 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
-
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate { //Inheritances required
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var desc: UITextField!
+    //Object used to allow user to take photo or select from gallery
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self //This class manages photo selection
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func selectPhoto(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum  //Select type of image selection
+        present(imagePicker, animated: true, completion: nil)
     }
-    */
-
+    
+    //Recover selected photo
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let recoveredImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        image.image = recoveredImage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 }
