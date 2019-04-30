bb0f5d5b9a3002ac7dc5b45bcdf068435c344df5
Eduardo <eduardomarques@outlook.com>
2019-04-30 02:02:53 -0300

    :rocket: Removing snap and images from the database after viewing

--- Snapchat/ViewControllers/DetailSnapsViewController.swift
+++ Snapchat/ViewControllers/DetailSnapsViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import SDWebImage
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
 class DetailSnapsViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
@@ -23,7 +26,30 @@ class DetailSnapsViewController: UIViewController {
         let url = URL(string: snap.urlImagem)
         image.sd_setImage(with: url) { (image, error, cache, url) in
             if error == nil {
-                print("imagem exibida")
+                
+            }
+        }
+    }
+    
+    //Method called whenever the view is closed
+    override func viewWillDisappear(_ animated: Bool) {
+        let auth = Auth.auth()
+        if let userLoggedId = auth.currentUser?.uid {
+            //Remove node in database
+            let users = Database.database().reference().child("usuarios")
+            let snaps = users.child(userLoggedId).child("snaps")
+            
+            snaps.child(snap.identificador).removeValue() //Remove snap from database
+            
+            //remove snap image
+            let images = Storage.storage().reference().child("imagens")
+            
+            images.child("\(snap.idImagem).jpg").delete { (error) in
+                if error == nil {
+                    print("Sucesso ao excluir a imagem")
+                } else {
+                    print("Erro ao excluir a imagem")
+                }
             }
         }
     }
